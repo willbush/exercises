ecdc19fe9d7d6d664f0135e0173d2c483153448d
Will Bush <will.g.bush@gmail.com>
2019-12-11 02:55:15 -0600

    add day1 AOC solutions in Haskell and Rust

---
+++ aoc/haskell/test/Spec.hs
@@ -0,0 +1,19 @@
+import           Test.Hspec
+import           Day1                           ( calcFuel
+                                                , calcModuleFuel
+                                                )
+
+main :: IO ()
+main = hspec $ do
+  describe "Part 1 Calc Fuel function" $ do
+    it "should do basic calculations" $ do
+      calcFuel 12 `shouldBe` 2
+      calcFuel 14 `shouldBe` 2
+      calcFuel 1969 `shouldBe` 654
+      calcFuel 100756 `shouldBe` 33583
+  describe "Part 2 Calc Fuel function" $ do
+    it "should do recursive calculations for the fuel of the fuel" $ do
+      calcModuleFuel 12 `shouldBe` 2
+      calcModuleFuel 14 `shouldBe` 2
+      calcModuleFuel 1969 `shouldBe` 966
+      calcModuleFuel 100756 `shouldBe` 50346
