4c50e922e53bb2df6a6d110ea5573aeb9e252f78
Will Bush <will.g.bush@gmail.com>
2019-12-25 23:27:39 -0600

    break up tests into separate files

---
+++ aoc/haskell/test/Day4Spec.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day4Spec (spec) where
+
+import           Day4                           ( meetsCriteria
+                                                , meetsCriteria'
+                                                , toDigits
+                                                )
+import           Test.Hspec
+
+spec :: Spec
+spec = describe "AOC Day 4" $ do
+  describe "Part 1 toDigits function"
+    $ it "turns a number into a list of digits."
+    $ do
+        toDigits 1 `shouldBe` [1]
+        toDigits 123 `shouldBe` [1, 2, 3]
+  describe "Part 1 meetsCriteria function" $ do
+    it "returns False for numbers that do not meet criteria." $ do
+      meetsCriteria 1 `shouldBe` False -- does not have an adjacent same pair
+      meetsCriteria 110 `shouldBe` False -- 0 is decreasing
+      meetsCriteria 1213 `shouldBe` False -- does not have an adjacent same pair
+    it "returns true if the given number meets the criteria." $ do
+      meetsCriteria 11 `shouldBe` True
+      meetsCriteria 22 `shouldBe` True
+      meetsCriteria 112 `shouldBe` True
+      meetsCriteria 11233 `shouldBe` True
+      meetsCriteria 12344 `shouldBe` True
+  describe "Part 2 meetsCriteria' function" $ do
+    it "returns False for numbers that do not meet criteria." $ do
+      meetsCriteria' 1 `shouldBe` False -- does not have an adjacent same pair
+      meetsCriteria' 110 `shouldBe` False -- 0 is decreasing
+      meetsCriteria' 1213 `shouldBe` False -- does not have an adjacent same pair
+      meetsCriteria' 123444 `shouldBe` False -- pair is not alone
+      meetsCriteria' 444555 `shouldBe` False -- pair is not alone
+      meetsCriteria' 4444 `shouldBe` False -- pair is not alone
+    it "returns true if the given number meets the criteria." $ do
+      meetsCriteria' 11 `shouldBe` True
+      meetsCriteria' 22 `shouldBe` True
+      meetsCriteria' 112 `shouldBe` True
+      meetsCriteria' 11233 `shouldBe` True
+      meetsCriteria' 12344 `shouldBe` True
+      meetsCriteria' 112233 `shouldBe` True
+      meetsCriteria' 111122 `shouldBe` True
+      meetsCriteria' 223333 `shouldBe` True
+
