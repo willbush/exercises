0c6ae50c20a415a8056eddcc036d95cc91c278b8
willbush <will.g.bush@gmail.com>
2019-12-25 23:27:39 -0600

    break up tests into separate files

---
+++ aoc/haskell/test/Day3Spec.hs
@@ -0,0 +1,170 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day3Spec (spec) where
+
+import           Data.List.HT                   ( mapAdjacent )
+import           Day3                           ( Direction(..)
+                                                , Line(..)
+                                                , findIntersection
+                                                , findIntersections
+                                                , manhattanDistance
+                                                , parseWireStr
+                                                , toCoords
+                                                , snipLoops
+                                                , stepsToIntersection
+                                                , insertWhen
+                                                , isPtOnLine
+                                                )
+import           Test.Hspec
+import           Safe.Foldable                  ( minimumMay )
+import           Data.Foldable                  ( foldl' )
+import qualified Data.ByteString.Char8         as B
+import qualified Data.Set                      as Set
+
+spec :: Spec
+spec =
+  describe "AOC Day 3" $ do
+    describe "Part 1 parse wire function"
+      $ it "should parse and drop invalid input"
+      $ do
+          parseWireStr "" `shouldBe` []
+          parseWireStr "R18,U42,L33" `shouldBe` [(R, 18), (U, 42), (L, 33)]
+          parseWireStr "R18,X42,L33" `shouldBe` [(R, 18), (L, 33)]
+          parseWireStr "R18,UX,L33" `shouldBe` [(R, 18), (L, 33)]
+    describe "Part 1 find intersection function"
+      $ it
+          "finds the intersection point between two line segments on a 2D plane."
+      $ do
+        -- ...|....
+        -- ...|....
+        -- ..-X---.
+        -- ...|....
+        -- ........
+        -- o.......
+        -- In the above o represents the origin of a Cartesian plane. Two line
+        -- segments intersect at the point X.
+          findIntersection (Line (3, 5) (3, 2)) (Line (6, 3) (2, 3))
+            `shouldBe` Just (3, 3)
+          -- ..........
+          -- ......|...
+          -- ......|...
+          -- ...---X--.
+          -- ......|...
+          -- ......|...
+          -- ..........
+          -- ..........
+          -- o.........
+          -- In the above o represents the origin of a Cartesian plane. Two line
+          -- segments intersect at the point X.
+          findIntersection (Line (8, 5) (3, 5)) (Line (6, 7) (6, 3))
+            `shouldBe` Just (6, 5)
+
+          -- ...........
+          -- .-------...
+          -- ...........
+          -- .........|.
+          -- .........|.
+          -- .........|.
+          -- .........|.
+          -- .........|.
+          -- .o.......|.
+          -- ...........
+          -- In the above o represents the origin of a Cartesian plane.
+          -- Note the line segments do not intersect.
+          findIntersection (Line (8, 0) (8, 5)) (Line (0, 7) (6, 7))
+            `shouldBe` Nothing
+
+    describe "Part 1 to coordinates function"
+      $ it
+          "translates a list of directions and amounts to points in a cartesian plane"
+      $ do
+          toCoords [] `shouldBe` []
+          toCoords [(U, 1)] `shouldBe` [(0, 1)]
+          toCoords [(U, 1), (R, 1)] `shouldBe` [(0, 1), (1, 1)]
+          toCoords [(U, 1), (R, 1), (D, 11)]
+            `shouldBe` [(0, 1), (1, 1), (1, -10)]
+
+    describe "Part 1 distances of the closest intersection function"
+      $ it "finds the distances of the closest intersection to the origin."
+      $ do
+          findClosestIntersectionDist "R8,U5,L5,D3" "U7,R6,D4,L4"
+            `shouldBe` Just 6
+          findClosestIntersectionDist "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+                                      "U62,R66,U55,R34,D71,R55,D58,R83"
+            `shouldBe` Just 159
+          findClosestIntersectionDist
+              "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+              "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            `shouldBe` Just 135
+
+    describe "insert when function" $
+      it "inserts between any two elements when the given predicate is true"
+        $ do
+            insertWhen (7 :: Int) (==) [] `shouldBe` []
+            insertWhen (7 :: Int) (==) [1] `shouldBe` [1]
+            insertWhen (7 :: Int) (==) [1, 2, 3] `shouldBe` [1, 2, 3]
+            insertWhen (7 :: Int) (==) [1, 2, 2, 3] `shouldBe` [1, 2, 7, 2, 3]
+            insertWhen (7 :: Int) (==) [1, 2, 2, 3, 3]
+              `shouldBe` [1, 2, 7, 2, 3, 7, 3]
+
+    describe "Part 2 snip loops function" $ do
+      it "it removes loops in the wire" $ do
+        snipLoops "" `shouldBe` ""
+        snipLoops "a" `shouldBe` "a"
+        snipLoops "aba" `shouldBe` "a"
+        snipLoops "aarsta" `shouldBe` "a"
+        snipLoops "aabbta" `shouldBe` "a"
+        snipLoops "abcbdedf" `shouldBe` "abdf"
+        snipLoops "abcbad" `shouldBe` "ad"
+        snipLoops "abba" `shouldBe` "a"
+        snipLoops "abcdcba" `shouldBe` "a"
+      it "it does not change the list when no duplicates exists"
+        $          snipLoops "abcd"
+        `shouldBe` "abcd"
+
+    describe "Part 2 find steps  to intersection function"
+      $ it "snips loops and counts the steps to an intersection"
+      $ do
+          findStepsToIntersection "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+                                  "U62,R66,U55,R34,D71,R55,D58,R83"
+            `shouldBe` Just 610
+          findStepsToIntersection
+              "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+              "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            `shouldBe` Just 410
+
+-- | Uses the library functions together to solve part 1 of Day3. I could have
+-- put this in the library, but I don't really need it there the intermediate
+-- results are used for part 2.
+findClosestIntersectionDist :: B.ByteString -> B.ByteString -> Maybe Int
+findClosestIntersectionDist wireStr1 wireStr2 =
+  let wireA = mapAdjacent Line $ (toCoords . parseWireStr) wireStr1
+      wireB = mapAdjacent Line $ (toCoords . parseWireStr) wireStr2
+  in  minimumMay $ manhattanDistance <$> findIntersections wireA wireB
+
+
+-- | Uses the library functions together to solve part 2 of Day3.
+findStepsToIntersection :: B.ByteString -> B.ByteString -> Maybe Int
+findStepsToIntersection wireStr1 wireStr2 =
+  let
+    wireDirections1      = parseWireStr wireStr1
+    wireDirections2      = parseWireStr wireStr2
+    wireCoords1          = snipLoops $ toCoords wireDirections1
+    wireCoords2          = snipLoops $ toCoords wireDirections2
+    wireA                = mapAdjacent Line wireCoords1
+    wireB                = mapAdjacent Line wireCoords2
+    intersectionsWithinA = Set.fromList $ findIntersections wireA wireA
+    intersectionsWithinB = Set.fromList $ findIntersections wireB wireB
+    intersections        = findIntersections wireA wireB
+    noLoopCoords1        = snipLoops $ foldl'
+      (\cs x -> insertWhen x (isPtOnLine x) cs)
+      wireCoords1
+      intersectionsWithinA
+    noLoopCoords2 = snipLoops $ foldl'
+      (\cs x -> insertWhen x (isPtOnLine x) cs)
+      wireCoords2
+      intersectionsWithinB
+    stepsPerIntersection1 = stepsToIntersection noLoopCoords1 <$> intersections
+    stepsPerIntersection2 = stepsToIntersection noLoopCoords2 <$> intersections
+  in
+    minimumMay $ uncurry (+) <$> zip stepsPerIntersection1 stepsPerIntersection2
---
+++ aoc/haskell/test/Day5Spec.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day5Spec (spec) where
+
+import           Day5                           ( runProgram )
+import           Test.Hspec
+
+spec :: Spec
+spec = describe "AOC Day 5" $ do
+  describe "Part 1 runProgram function" $ do
+    it "can return nothing" $ do
+      runProgram 1 [99] `shouldBe` []
+      runProgram 1 [99, 0, 0, 0, 1, 0, 0, 0] `shouldBe` []
+      runProgram 1 [] `shouldBe` []
+    it "can add and output" $ do
+      runProgram 1 [1, 0, 0, 0, 4, 0, 99] `shouldBe` [2]
+      runProgram 1 [1101, 2, 2, 0, 4, 0, 99] `shouldBe` [4]
+      runProgram 1 [1101, 100, -1, 0, 4, 0, 99] `shouldBe` [99]
+    it "can multiply and output" $ do
+      runProgram 1 [2, 0, 0, 0, 4, 0, 99] `shouldBe` [4]
+      runProgram 1 [0002, 0, 0, 0, 4, 0, 99] `shouldBe` [4]
+      runProgram 1 [1102, 4, 4, 0, 4, 0, 99] `shouldBe` [16]
+    it "can output immediate values" $ runProgram 1 [104, 7, 99] `shouldBe` [7]
+  describe "Part 2 runProgram function" $ do
+    it "compares input is equal to 8 using position mode." $ do
+      let program = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+      runProgram 7 program `shouldBe` [0]
+      runProgram 2 program `shouldBe` [0]
+      runProgram 8 program `shouldBe` [1]
+    it "compares input is less than 8 using position mode." $ do
+      let program = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+      runProgram 7 program `shouldBe` [1]
+      runProgram 1 program `shouldBe` [1]
+      runProgram 8 program `shouldBe` [0]
+      runProgram 10 program `shouldBe` [0]
+    it "compares input is equal to 8 using immediate mode." $ do
+      let program = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+      runProgram 7 program `shouldBe` [0]
+      runProgram 8 program `shouldBe` [1]
+    it "compares input is less than 8 using immediate mode." $ do
+      let program = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+      runProgram 7 program `shouldBe` [1]
+      runProgram 8 program `shouldBe` [0]
+    it "tests jump using position mode" $ do
+      -- | outputs 0 if input was 0 and 1 if input was non-zero
+      let program = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
+      runProgram 1 program `shouldBe` [1]
+      runProgram 0 program `shouldBe` [0]
+    it "tests jump using immediate mode" $ do
+      -- | outputs 0 if input was 0 and 1 if input was non-zero
+      let program = [3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1]
+      runProgram 123 program `shouldBe` [1]
+      runProgram 0 program `shouldBe` [0]
+    it "tests longer program" $ do
+      -- | The program will then output 999 if the input value is below 8,
+      -- output 1000 if the input value is equal to 8, or output 1001 if the
+      -- input value is greater than 8.
+      let program =
+            [ 3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8
+            , 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0
+            , 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999
+            , 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105
+            , 1, 46, 98, 99
+            ]
+      runProgram 8 program `shouldBe` [1000]
+      runProgram 800 program `shouldBe` [1001]
+
