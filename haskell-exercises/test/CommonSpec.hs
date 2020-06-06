27a88f9dd30d8686ed31885857c4ce6b1ccb2694
willbush <will.g.bush@gmail.com>
2020-06-06 02:31:28 -0500

    haskell: minor refactor

--- haskell-exercises/test/CommonSpec.hs
+++ haskell-exercises/test/CommonSpec.hs
@@ -8,8 +8,10 @@ spec =
   describe "Common Library Functions" $
     describe "Fibonacci" $ do
     it "can generate the first 12 terms of the fibonacci sequence" $ do
-      fmap fibFast [0 .. 11] `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-      fmap fibSlow [0 .. 11] `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+      let expected = [0 , 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+          inputs = [0 .. 11]
+      fmap fibFast inputs `shouldBe` expected
+      fmap fibSlow inputs `shouldBe` expected
     it "returns any given negative number" $ do
       fibFast (-100) `shouldBe` (-100)
       fibSlow (-666) `shouldBe` (-666)
