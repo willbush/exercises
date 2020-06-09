49812d03002a616ab8208e392ea4376806a614b0
Will Bush <will.g.bush@gmail.com>
2020-06-08 20:43:06 -0500

    haskell: make accumulator strict

--- haskell-exercises/src/Common/Helpers.hs
+++ haskell-exercises/src/Common/Helpers.hs
@@ -1,12 +1,11 @@
--- {-# LANGUAGE Strict #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Common.Helpers where
-
 
 reverseInt :: Int -> Int
 reverseInt n | n < 0     = -go 0 (-n)
                | otherwise = go 0 n
  where
   go :: Int -> Int -> Int
-  go acc 0 = acc
-  go acc x = let (q, r) = quotRem x 10 in go (acc * 10 + r) q
+  go !acc 0 = acc
+  go !acc x = let (q, r) = quotRem x 10 in go (acc * 10 + r) q
