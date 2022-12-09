86e60f6c6d08ba7c7e328b050012bc5777212e31
Will Bush <will.g.bush@gmail.com>
2022-12-08 22:35:45 -0600

    rust: day 4 part 1

---
+++ rust-aoc-2022/src/day4.rs
@@ -0,0 +1,42 @@
+/// https://adventofcode.com/2022/day/4
+use std::{fs::File, io::BufReader};
+
+use crate::utils::parse_lines;
+
+pub fn run() -> std::io::Result<()> {
+    let file = File::open("../inputs/aoc/2022/day4.txt")?;
+    let mut reader = BufReader::new(file);
+    let pattern = [',', '-'];
+
+    // The number of assignment pairs that fully contain the other.
+    let mut total_fully_contain_ranges = 0;
+
+    parse_lines(&mut reader, 20, |line| {
+        let mut xs = line.split(&pattern);
+        let numbers = (
+            xs.next().map(|x| x.parse::<usize>()).and_then(Result::ok),
+            xs.next().map(|x| x.parse::<usize>()).and_then(Result::ok),
+            xs.next().map(|x| x.parse::<usize>()).and_then(Result::ok),
+            xs.next().map(|x| x.parse::<usize>()).and_then(Result::ok),
+        );
+
+        match numbers {
+            (Some(a), Some(b), Some(x), Some(y)) => {
+                if (a <= x && b >= y) || (x <= a && y >= b) {
+                    total_fully_contain_ranges += 1;
+                }
+            }
+            _ => panic!("Invalid input"),
+        }
+    });
+
+    println!("Day 4");
+    println!("part 1: {}", total_fully_contain_ranges);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod day4_tests {
+    use super::*;
+}
--- rust-aoc-2022/src/main.rs
+++ rust-aoc-2022/src/main.rs
@@ -1,11 +1,13 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod utils;
 
 fn main() -> std::io::Result<()> {
     day1::run()?;
     day2::run()?;
     day3::run()?;
+    day4::run()?;
     Ok(())
 }
