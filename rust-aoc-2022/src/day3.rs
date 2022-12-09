49053adbd298ef2bcfe2721bcbd66b3ab0f2b58a
Will Bush <will.g.bush@gmail.com>
2022-12-08 18:21:52 -0600

    rust: finish day 3 part 2

--- rust-aoc-2022/src/day3.rs
+++ rust-aoc-2022/src/day3.rs
@@ -2,20 +2,19 @@
 use std::{
     collections::HashSet,
     fs::File,
-    io::BufReader,
+    io::{BufReader, Seek, SeekFrom},
 };
 
-use crate::utils::parse_lines;
+use crate::utils::{parse_lines, parse_lines_by3};
 
 pub fn run() -> std::io::Result<()> {
     let file = File::open("../inputs/aoc/2022/day3.txt")?;
     let mut reader = BufReader::new(file);
 
-    // let result = parse(&mut reader);
-
-    let mut result = 0;
+    let mut part1_result = 0;
 
     parse_lines(&mut reader, 100, |line| {
+        // part 1
         let items = line.chars().collect::<Vec<_>>();
         let mid = items.len() / 2;
 
@@ -23,14 +22,37 @@ pub fn run() -> std::io::Result<()> {
         let snd: HashSet<&char> = HashSet::from_iter(items[mid..].iter());
 
         for &item in fst.intersection(&snd) {
-            result += priority(*item);
+            part1_result += priority(*item);
         }
     });
 
     println!("Day 3");
-    println!("part 1: {}", result);
+    println!("part 1: {}", part1_result);
+
+    let mut part2_result = 0;
+
+    reader.seek(SeekFrom::Start(0))?;
+
+    parse_lines_by3(&mut reader, 100, |line1, line2, line3| {
+        for item in intersect(line1, line2, line3) {
+            part2_result += priority(item);
+        }
+    });
+
+    println!("part 2: {}", part2_result);
 
     Ok(())
+}
+
+fn intersect(line1: &str, line2: &str, line3: &str) -> HashSet<char> {
+    let xs: HashSet<char> = HashSet::from_iter(line1.chars());
+    let ys: HashSet<char> = HashSet::from_iter(line2.chars());
+    let zs: HashSet<char> = HashSet::from_iter(line3.chars());
+
+    xs.iter()
+        .filter(|x| ys.contains(x) && zs.contains(x))
+        .cloned()
+        .collect()
 }
 
 // Calculate the priority for a given char (item).
@@ -51,8 +73,18 @@ mod day1_tests {
     #[test]
     fn priority_test() {
         assert_eq!(priority('a'), 1);
+        assert_eq!(priority('b'), 2);
         assert_eq!(priority('z'), 26);
         assert_eq!(priority('A'), 27);
         assert_eq!(priority('Z'), 52);
+    }
+
+    #[test]
+    fn intersect_test() {
+        let line1 = "vJrwpWtwJgWrhcsFMMfFFhFp";
+        let line2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
+        let line3 = "PmmdzqPrVvPwwTWBwg";
+
+        assert_eq!(intersect(line1, line2, line3), HashSet::from(['r']));
     }
 }
--- rust-aoc-2022/src/utils.rs
+++ rust-aoc-2022/src/utils.rs
@@ -32,3 +32,50 @@ where
         line.clear();
     }
 }
+
+/// Apply the given parse function to 3 lines at a time from the reader until
+/// EOF. Line endings are trimmed off. Uses a string buffer of the given
+/// capacity to avoid string allocation for each line.
+///
+/// Note that lines at the end of the file that are now a multiple of 3 are
+/// dropped.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// parse_lines_by3(&mut reader, 100, |line1, line2, line3| {
+///     println!("{}, {}, {}", line1, line2, line3);
+/// });
+/// ```
+pub fn parse_lines_by3<R, F>(reader: &mut R, capacity: usize, mut parser: F)
+where
+    R: BufRead,
+    F: FnMut(&str, &str, &str),
+{
+    let mut a = String::with_capacity(capacity);
+    let mut b = String::with_capacity(capacity);
+    let mut c = String::with_capacity(capacity);
+
+    loop {
+        let bytes_read_tuple = (
+            reader.read_line(&mut a),
+            reader.read_line(&mut b),
+            reader.read_line(&mut c),
+        );
+
+        match bytes_read_tuple {
+            (Ok(0), Ok(_), Ok(_)) => break, // EOF reached
+            (Ok(_), Ok(0), Ok(_)) => break, // EOF reached
+            (Ok(_), Ok(_), Ok(0)) => break, // EOF reached
+            (Ok(_), Ok(_), Ok(_)) => {
+                parser(&a.trim_end(), &b.trim_end(), &c.trim_end());
+                a.clear();
+                b.clear();
+                c.clear();
+            }
+            _ => break,
+        }
+    }
+}
