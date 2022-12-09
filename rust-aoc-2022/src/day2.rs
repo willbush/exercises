783be93e23b62e53bb3d3731908438ffc6f65b10
Will Bush <will.g.bush@gmail.com>
2022-12-08 20:04:27 -0600

    rust: finish day 2 part 2

--- rust-aoc-2022/src/day2.rs
+++ rust-aoc-2022/src/day2.rs
@@ -9,10 +9,14 @@ use crate::utils::parse_lines;
 pub fn run() -> std::io::Result<()> {
     let file = File::open("../inputs/aoc/2022/day2.txt")?;
     let mut reader = BufReader::new(file);
-    let my_total_score: Score = parse_to_my_scores(&mut reader).iter().sum();
+
+    let scores = parse_to_my_scores(&mut reader);
+    let fst_strat_total_score: Score = scores.iter().map(|x| x.0).sum();
+    let snd_strat_total_score: Score = scores.iter().map(|x| x.1).sum();
 
     println!("Day 2");
-    println!("part 1: {}", my_total_score);
+    println!("part 1: {}", fst_strat_total_score);
+    println!("part 2: {}", snd_strat_total_score);
 
     Ok(())
 }
@@ -25,7 +29,13 @@ enum RPS {
     Scissors,
 }
 
-fn parse_to_my_scores<R>(reader: &mut R) -> Vec<Score>
+enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+fn parse_to_my_scores<R>(reader: &mut R) -> Vec<(Score, Score)>
 where
     R: BufRead,
 {
@@ -34,9 +44,19 @@ where
     parse_lines(reader, 5, |line| {
         let cs = line.chars().collect::<Vec<_>>();
 
-        if let (Some(&theirs), Some(&my)) = (cs.first(), cs.last()) {
-            if let (Some(their_rps), Some(my_rps)) = (char_to_rps(theirs), char_to_rps(my)) {
-                scores.push(calc_my_score(&their_rps, &my_rps));
+        if let (Some(&theirs), Some(&last)) = (cs.first(), cs.last()) {
+            // Part 1 strategy
+            if let (Some(their_rps), Some(my_rps), Some(outcome)) = (
+                char_to_rps(theirs),
+                char_to_rps(last),
+                char_to_outcome(last),
+            ) {
+                // Part 1 strategy
+                let fst_strat_score = calc_my_score(&their_rps, &my_rps);
+                // Part 2 strategy
+                let snd_strat_score = calc_my_score(&their_rps, &get_my_move(&their_rps, &outcome));
+
+                scores.push((fst_strat_score, snd_strat_score));
             }
         }
     });
@@ -50,6 +70,31 @@ fn char_to_rps(c: char) -> Option<RPS> {
         'B' | 'Y' => Some(RPS::Paper),
         'C' | 'Z' => Some(RPS::Scissors),
         _ => None,
+    }
+}
+
+fn char_to_outcome(c: char) -> Option<Outcome> {
+    match c {
+        'X' => Some(Outcome::Lose),
+        'Y' => Some(Outcome::Draw),
+        'Z' => Some(Outcome::Win),
+        _ => None,
+    }
+}
+
+fn get_my_move(their: &RPS, outcome: &Outcome) -> RPS {
+    match (their, outcome) {
+        (RPS::Rock, Outcome::Win) => RPS::Paper,
+        (RPS::Rock, Outcome::Draw) => RPS::Rock,
+        (RPS::Rock, Outcome::Lose) => RPS::Scissors,
+
+        (RPS::Paper, Outcome::Win) => RPS::Scissors,
+        (RPS::Paper, Outcome::Draw) => RPS::Paper,
+        (RPS::Paper, Outcome::Lose) => RPS::Rock,
+
+        (RPS::Scissors, Outcome::Win) => RPS::Rock,
+        (RPS::Scissors, Outcome::Draw) => RPS::Scissors,
+        (RPS::Scissors, Outcome::Lose) => RPS::Paper,
     }
 }
 
@@ -72,14 +117,31 @@ mod day1_tests {
     use super::*;
 
     #[test]
-    fn can_calc_score() {
+    fn can_calc_score_part1() {
         let input = "A Y
 B X
 C Z";
 
         let mut reader = BufReader::new(input.as_bytes());
-        let scores = parse_to_my_scores(&mut reader);
+        let scores = parse_to_my_scores(&mut reader)
+            .iter()
+            .map(|x| x.0)
+            .collect::<Vec<_>>();
         assert_eq!(vec![8, 1, 6], scores);
+    }
+
+    #[test]
+    fn can_calc_score_part2() {
+        let input = "A Y
+B X
+C Z";
+
+        let mut reader = BufReader::new(input.as_bytes());
+        let scores = parse_to_my_scores(&mut reader)
+            .iter()
+            .map(|x| x.1)
+            .collect::<Vec<_>>();
+        assert_eq!(vec![4, 1, 7], scores);
     }
 
     #[test]
@@ -89,7 +151,10 @@ A Z
 A Y ";
 
         let mut reader = BufReader::new(input.as_bytes());
-        let scores = parse_to_my_scores(&mut reader);
+        let scores = parse_to_my_scores(&mut reader)
+            .iter()
+            .map(|x| x.0)
+            .collect::<Vec<_>>();
         assert_eq!(vec![1, 3, 8], scores);
     }
 
@@ -108,7 +173,10 @@ C Y
 B Y";
 
         let mut reader = BufReader::new(input.as_bytes());
-        let scores = parse_to_my_scores(&mut reader);
+        let scores = parse_to_my_scores(&mut reader)
+            .iter()
+            .map(|x| x.0)
+            .collect::<Vec<_>>();
         assert_eq!(vec![2, 6, 5, 5, 5, 8, 2, 5, 2, 2, 5], scores);
     }
 }
